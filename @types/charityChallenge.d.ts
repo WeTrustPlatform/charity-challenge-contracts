bf332d7f063c804e435bb54e643aba5d4d20c8c0
superdevjohnnash <superdevjohnnash@gmail.com>
2018-11-14 10:53:01 +0700

    Add constructor

--- @types/charityChallenge.d.ts
+++ @types/charityChallenge.d.ts
@@ -1,5 +1,9 @@
 import { TransactionOptions } from "./contracts"
 
 export interface CharityChallenge {
-  contractOwner(): Promise<string>
+  contractOwner(): Promise<string>;
+
+  npoAddress(): Promise<string>;
+
+  challengeName(): Promise<string>;
 }
--- test/CharityChallenge.test.js
+++ test/CharityChallenge.test.js
@@ -5,14 +5,27 @@ const CharityChallenge = artifacts.require('CharityChallenge.sol')
 
 contract('CharityChallenge', (accounts) => {
   const CONTRACT_OWNER = accounts[1]
+  const RAINFOREST_NPO_ADDRESS = accounts[2]
+  const VITALIK_WEARS_SUIT_CHALLENGE = 'Vitalik wearing suits on new year\'s eve'
 
   let charityChallengeContract
 
   beforeEach(async () => {
-    charityChallengeContract = await CharityChallenge.new(CONTRACT_OWNER)
+    charityChallengeContract = await CharityChallenge.new(
+      CONTRACT_OWNER,
+      RAINFOREST_NPO_ADDRESS,
+      VITALIK_WEARS_SUIT_CHALLENGE)
   })
 
   it('should set contract owner via constructor', async () => {
     assert.equal(await charityChallengeContract.contractOwner(), CONTRACT_OWNER)
+  })
+
+  it('should set NPO address via constructor', async () => {
+    assert.equal(await charityChallengeContract.npoAddress(), RAINFOREST_NPO_ADDRESS)
+  })
+
+  it('should set challenge name via constructor', async () => {
+    assert.equal(await charityChallengeContract.challengeName(), VITALIK_WEARS_SUIT_CHALLENGE)
   })
 })
