b289a3f6e04d7a210cd719836c3ea85506728e70
superdevjohnnash <superdevjohnnash@gmail.com>
2018-11-14 12:03:08 +0700

    Format and check styles

--- @types/charityChallenge.d.ts
+++ @types/charityChallenge.d.ts
@@ -1,9 +1,11 @@
-import { TransactionOptions } from "./contracts"
-
 export interface CharityChallenge {
   contractOwner(): Promise<string>;
 
   npoAddress(): Promise<string>;
 
   challengeName(): Promise<string>;
+
+  marketAddress(): Promise<string>;
+
+  checkAugur(): Promise<boolean[]>;
 }
