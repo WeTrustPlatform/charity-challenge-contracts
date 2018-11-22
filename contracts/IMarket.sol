60a093a46088c43eaa40e94d7351ea1649272770
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-11-22 14:49:59 +0700

    Don't expose setters used in tests

--- contracts/IMarket.sol
+++ contracts/IMarket.sol
@@ -7,7 +7,4 @@ contract IMarket {
     function isFinalized() public view returns (bool) {}
     function isInvalid() public view returns (bool) {}
     function getWinningPayoutNumerator(uint256 _outcome) public view returns (uint256) {}
-    function setFinalized(bool _finalized) public {}
-    function setInvalid(bool _invalid) public {}
-    function setPayountNumerators(uint32[2] _payoutNumerators) public {}
 }
