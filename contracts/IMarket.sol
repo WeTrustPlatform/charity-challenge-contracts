53823d57599918d820f8239b0bdc5c3f00ec83d1
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-11-23 10:17:35 +0700

    Define IMarket as an interface

--- contracts/IMarket.sol
+++ contracts/IMarket.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.24;
 // Market API used to interract with augur, we only need to describe the
 // functions we'll be using.
 // cf https://github.com/AugurProject/augur-core/blob/master/source/contracts/reporting/IMarket.sol
-contract IMarket {
-    function isFinalized() public view returns (bool) {}
-    function isInvalid() public view returns (bool) {}
-    function getWinningPayoutNumerator(uint256 _outcome) public view returns (uint256) {}
+interface IMarket {
+    function isFinalized() external view returns (bool);
+    function isInvalid() external view returns (bool);
+    function getWinningPayoutNumerator(uint256 _outcome) external view returns (uint256);
 }
