07cd6e6de2b076c81589bd46f1b472cf3f2e0a2f
Jean-André Santoni <jean.andre.santoni@gmail.com>
2019-05-31 14:01:10 +0700

    Update exported RealityCheckMock

--- dist.js
+++ dist.js
@@ -2,14 +2,14 @@ const path = require('path');
 const fs = require('fs');
 
 const CharityChallengeContract = path.resolve(__dirname, './build/contracts/CharityChallenge.json');
-const MarketMockContract = path.resolve(__dirname, './build/contracts/MarketMock.json');
+const RealityCheckMockContract = path.resolve(__dirname, './build/contracts/RealityCheckMock.json');
 const TestableCharityChallengeContract = path.resolve(__dirname, './build/contracts/TestableCharityChallenge.json');
 
 const getSubContent = ({abi, bytecode, sourceMap, source, compiler, schemaVersion}) => ({abi, bytecode, sourceMap, source, compiler, schemaVersion})
 
 const contractsContent = {
   CharityChallenge: getSubContent(require(CharityChallengeContract)),
-  MarketMock: getSubContent(require(MarketMockContract)),
+  RealityCheckMock: getSubContent(require(RealityCheckMockContract)),
   TestableCharityChallenge: getSubContent(require(TestableCharityChallengeContract))
 }
 
